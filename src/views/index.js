f2d7ae1d0b65f740be1d77eea0b2480cae71f9da
Benjamin Torio <beto3809@colorado.edu>
2026-03-27 16:36:24 -0600

    update to include express

--- src/views/index.js
+++ src/views/index.js
@@ -55,6 +55,7 @@ app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(bodyParser.json()); // specify the usage of JSON for parsing request body.
+app.use(express.static(path.join(__dirname, 'resources')));
 
 // initialize session variables
 app.use(
